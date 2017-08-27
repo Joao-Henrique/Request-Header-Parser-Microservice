da010cc9e9676beb576bf911546a477e9e05379a
Joao Henrique <jppulicidade@gamail.com>
2017-08-27 21:38:21 +0100

    all good for GLITCH deployment

--- server.js
+++ server.js
@@ -2,63 +2,41 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var path = require('path');
+
+//TO EASILY GET THE OPERATING USER OPERATING SYSTEM VALUE
+var useragent = require('express-useragent');
 
 //CREATE AN INSTANCE OF EXPRESS FOR OUR APP AND ISNTANTIATE BODYPARSER AND CORS
 var app = module.exports = express();
 app.use(bodyParser.json());
-app.use(cors);
+app.use(cors());
+app.use(useragent.express());
+app.use("/", express.static(__dirname));
 
-//API URL
-var api = "/api/whoami";
-
-
-
-if (!process.env.DISABLE_XORIGIN) {
-  app.use(function(req, res, next) {
-    var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
-    var origin = req.headers.origin || '*';
-    if(!process.env.XORIG_RESTRICT || allowedOrigins.indexOf(origin) > -1){
-         console.log(origin);
-         res.setHeader('Access-Control-Allow-Origin', origin);
-         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    }
-    next();
-  });
-}
-
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.route('/_api/package.json')
-  .get(function(req, res, next) {
-    console.log('requested');
-    fs.readFile(__dirname + '/package.json', function(err, data) {
-      if(err) return next(err);
-      res.type('txt').send(data.toString());
-    });
-  });
-  
-app.route('/')
-    .get(function(req, res) {
-		  res.sendFile(process.cwd() + '/views/index.html');
-    })
-
-// Respond not found to all the wrong routes
-app.use(function(req, res, next){
-  res.status(404);
-  res.type('txt').send('Not found');
+//SEND HTML AND CSS TO THE CLIENT SIDE
+app.get('/', function(req, res){
+  res.sendFile("index.html", {root: __dirname})
+  res.sendFile("style.css", {root: __dirname})
 });
 
-// Error Middleware
-app.use(function(err, req, res, next) {
-  if(err) {
-    res.status(err.status || 500)
-      .type('txt')
-      .send(err.message || 'SERVER ERROR');
-  }  
+//API URL
+var api = "/api/whoami/";
+
+//GET REQUIRED INFO ABOUT THE USER
+app.get(api, function(req, res){
+  var ipAddress = req.ip;
+  var language = req.acceptsLanguages();
+  var software = req.useragent.platform;  
+  var operatingSystem = req.useragent.os;
+  var browser = req.useragent.browser;
+
+//SEND USER INFO IN JSON FORMAT
+  res.json({'ip Address': ipAddress, 'Language': language[0], 'Platform': software, 'OS': operatingSystem, 'Browser': browser});
 })
 
+//LISTEN ON PORT
 app.listen(3000, function(){
-//app.listen(process.env.PORT, function () {
   console.log('Your Server is Working...');
 });
 
